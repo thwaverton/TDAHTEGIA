475ec39fa7e6e9b1067e5c86712b7bf113652190
thwaverton <113214844+thwaverton@users.noreply.github.com>
2024-07-16 08:36:47 -0300

    a

---
+++ app/pages/termo_de_uso.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Exibir PDF em uma DIV</title>
+    <style>
+        body{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+        .conteiner {
+            width: 45%;
+            height: 50%;
+            overflow-y: scroll;
+            border: 1px solid #ccc;
+            padding: 20px;
+            box-sizing: border-box;
+            margin-top: 10%;
+            margin-bottom: 10%;
+        }
+        h2, h3 {
+            margin-top: 20px;
+        }
+        p {
+            text-align: justify;
+        }
+        ul {
+            list-style: none;
+            padding-left: 0;
+        }
+        li {
+            margin-bottom: 10px;
+        }
+        
+    </style>
+</head>
+<body>
+    <div class="conteiner">
+        <h2>Termos de Uso e Política de Privacidade</h2>
+        <p>Última atualização: [16/07/2023]</p>
+        
+        <h3>Termos de Uso</h3>
+        <h4>1. Aceitação dos Termos</h4>
+        <p>Ao utilizar o site ou aplicativo de estudo TDAHTÉGIA, você concorda em cumprir e estar sujeito a estes Termos de Uso. Se você não concordar com estes termos, por favor, não utilize a plataforma.</p>
+        
+        <h4>2. Elegibilidade</h4>
+        <p>O uso da plataforma é permitido apenas para indivíduos com idade mínima de 10 anos. Menores de 18 anos devem obter permissão de um responsável legal para utilização da mesma.</p>
+        
+        <h4>3. Conta de Usuário</h4>
+        <p>Você deve criar uma conta para acessar algumas funcionalidades da plataforma. Você é responsável por manter a confidencialidade de sua senha e por todas as atividades que ocorrem em sua conta.</p>
+        
+        <h4>4. Uso Aceitável</h4>
+        <p>Você concorda em usar o Aplicativo apenas para fins legais e de acordo com os Termos de Uso. Você não deve:</p>
+        <ul>
+            <li>Utilizar a plataforma de forma que viole qualquer lei ou regulamento local, estadual, nacional ou internacional.</li>
+            <li>Publicar, enviar ou compartilhar qualquer conteúdo que seja ilegal, ofensivo, difamatório, obsceno ou que infrinja os direitos de terceiros.</li>
+            <li>Tentar acessar contas de outros usuários ou dados da plataforma sem autorização.</li>
+        </ul>
+        
+        <h4>5. Direitos de Propriedade</h4>
+        <p>Todos os direitos, títulos e interesses relativos à plataforma e seu conteúdo são de propriedade exclusiva dos desenvolvedores. Você não pode copiar, modificar, distribuir ou criar obras derivadas com base na plataforma sem permissão explícita.</p>
+        
+        <h4>6. Alterações a esta Política</h4>
+        <p>Podemos atualizar esta Política de Privacidade periodicamente. Notificaremos você sobre quaisquer alterações significativas publicando a nova política na plataforma. Recomendamos revisar esta página regularmente para se manter informado sobre nossa coleta e uso de informações.</p>
+        
+        <h4>7. Contato</h4>
+        <p>Se você possuir qualquer dúvida sobre estes Termos de Uso e Política de Privacidade, entre em contato conosco em [approtinadeestudos@gmail.com].</p>
+
+        <p>Ao usar nosso Aplicativo, você confirma que leu e concorda com estes Termos de Uso e Política de Privacidade.</p>
+    </div>
+    <div id="footer">
+
+    </div>
+</body>
+</html>
